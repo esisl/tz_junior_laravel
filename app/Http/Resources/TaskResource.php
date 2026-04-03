f1b981286432ded567949e25bd34b3dfad010c53
esisl <esisl@yandex.ru>
2026-04-03 16:12:01 +0300

    tzj_laravel 03.04 7

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -8,12 +8,19 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class TaskResource extends JsonResource
 {
     /**
-     * Transform the resource into an array.
+     * Преобразование ресурса в массив.
      *
      * @return array<string, mixed>
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id'          => $this->id,
+            'title'       => $this->title,
+            'description' => $this->description,
+            'status'      => $this->status,
+            'created_at'  => $this->created_at?->toIso8601String(),
+            'updated_at'  => $this->updated_at?->toIso8601String(),
+        ];
     }
 }
